ee361038738fbbf5aeffb2b23cb53645f8b10161
sulthansugesti00-wq <sulthansugesti00@gmail.com>
2026-02-23 09:26:37 +0700

    Added the correct analysis
    - Added arithmethic stint 1 and 2 with For i loop
    - Added allocation of the result of for loop

--- tire_deg_model.R
+++ tire_deg_model.R
@@ -36,6 +36,34 @@ tire_deg_model <- fuel_corrected %>%
 # degradation rate slope
 deg_rate <- coef(tire_deg_model)[2] #[1] is intercept
 print(paste("Time lost due to deg per lap is~", round(deg_rate,2)))
+# Slope rate is ~ 0.07
 
-## When to pit ----
+# cumulative tire loss sn = n/2 (a1+an)
+cumulative_tire_loss <- fuel_corrected %>% 
+  mutate(an = deg_rate+(Lap-1)*deg_rate,
+    cumulative_loss = Lap/2*(deg_rate+an)) %>% 
+  select(Lap, current_fuel, corrected_laptime_fuel, cumulative_loss)
 
+# total laptime with fuel corrected
+aggregated_data <- cumulative_tire_loss %>% 
+  mutate(total_laptime=corrected_laptime_fuel+cumulative_loss) %>% 
+  select(Lap, total_laptime) %>% 
+  group_by(Lap) %>% 
+  summarise(total_laptime=mean(total_laptime))
+
+n <- nrow(aggregated_data)
+pit_time <- 22
+optimal_pit <- vector("numeric", length = (n-1))
+
+for (k in 1:(n-1)){
+  optimal_pit[k] <- sum(aggregated_data$total_laptime[1:k]) + sum(aggregated_data$total_laptime[1:(n-k)]) +pit_time
+}
+
+optimal_pit_data <- tibble(
+  total_time = c(optimal_pit),
+  pit_lap = c(1:(n-1)) #n-1 for easy change
+)
+
+# Plotting optimal pit
+ggplot(data=optimal_pit_data,aes(x=pit_lap, y=total_time))+geom_line(color="steelblue")+geom_vline(xintercept = which.min(optimal_pit_data$total_time),linetype="dashed"
+)
